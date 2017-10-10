0d4907ed26087adf41f2b2603f139f9d31789c60
Steve Brenneis <steve.brenneis@secomm.org>
2017-10-10 14:55:40 -0400

    Initial commit.

---
+++ pippip-native-iosTests/pippip_native_iosTests.m
@@ -0,0 +1,39 @@
+//
+//  pippip_native_iosTests.m
+//  pippip-native-iosTests
+//
+//  Created by Steve Brenneis on 10/9/17.
+//  Copyright © 2017 seComm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pippip_native_iosTests : XCTestCase
+
+@end
+
+@implementation pippip_native_iosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
