4046e38da7924f00b045f2df8e6329fd0508cec9
Steve Brenneis <steve.brenneis@secomm.org>
2017-10-10 15:37:43 -0400

    Initial commit.

---
+++ pippip-native-ios/RESTSession.h
@@ -0,0 +1,13 @@
+//
+//  RESTSession.h
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 10/10/17.
+//  Copyright © 2017 seComm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RESTSession : NSObject
+
+@end
---
+++ pippip-native-ios/RESTSession.m
@@ -0,0 +1,13 @@
+//
+//  RESTSession.m
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 10/10/17.
+//  Copyright © 2017 seComm. All rights reserved.
+//
+
+#import "RESTSession.h"
+
+@implementation RESTSession
+
+@end
