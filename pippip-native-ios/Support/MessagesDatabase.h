acfa416dd464e20263e9204ba128f4f5acc9d9dd
Steve Brenneis <steve.brenneis@secomm.org>
2018-02-04 11:18:29 -0500

    Initial commit.

---
+++ pippip-native-ios/Support/MessagesDatabase.h
@@ -0,0 +1,21 @@
+//
+//  MessagesDatabase.h
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 1/29/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SessionState.h"
+#import "DatabaseMessage.h"
+
+@interface MessagesDatabase : NSObject
+
+- (void)addMessage:(DatabaseMessage*)message;
+
+- (NSArray*)mostRecent;
+
+- (BOOL)loadMessages:(SessionState*)state;
+
+@end
