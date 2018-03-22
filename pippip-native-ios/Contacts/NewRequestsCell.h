037a3d54e8754507d270cf59853b1de785ffd1c6
Steve Brenneis <steve.brenneis@secomm.org>
2018-03-22 06:58:27 -0400

    Initial commit.

---
+++ pippip-native-ios/Contacts/NewRequestsCell.h
@@ -0,0 +1,15 @@
+//
+//  NewRequestsCell.h
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 3/21/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewRequestsCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *badgeCountLabel;
+
+@end
---
+++ pippip-native-ios/Contacts/NewRequestsCell.m
@@ -0,0 +1,24 @@
+//
+//  NewRequestsCell.m
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 3/21/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+#import "NewRequestsCell.h"
+
+@implementation NewRequestsCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
