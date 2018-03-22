9163f04c360b3810089f0be95c9a3d748298e6cb
Steve Brenneis <steve.brenneis@secomm.org>
2018-03-22 06:59:22 -0400

    Initial commit.

---
+++ pippip-native-ios/More/DeleteAccountCell.h
@@ -0,0 +1,16 @@
+//
+//  DeleteAccountCell.h
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 3/21/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MoreCellItem.h"
+
+@interface DeleteAccountCell : UITableViewCell
+
++ (MoreCellItem*)cellItem;
+
+@end
