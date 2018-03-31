913349629e41370c8aa8d5e098299818484dc4f7
Steve Brenneis <steve.brenneis@secomm.org>
2018-03-31 15:57:21 -0400

    Initial commit.

---
+++ pippip-native-ios/pippip-native-ios-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "CKRSAPrivateKey.h"
+#import "CKRSAPublicKey.h"
+#import "CKRSAKeyPair.h"
+#import "CKRSAKeyPairGenerator.h"
+#import "CKSecureRandom.h"
+#import "CKSHA256.h"
+#import "CKSHA1.h"
+#import "CKPEMCodec.h"
+#import "HexCodec.h"
+#import "MoreCellItem.h"
+#import "ApplicationSingleton.h"
+#import "ContactManager.h"
+#import "Configurator.h"
+#import "MBProgressHUD.h"
+#import "Authenticator.h"
+#import "NewAccountCreator.h"
+
