8fd92d72a7ce723e72639395c1dd1eea4d037b6a
Steve Brenneis <steve.brenneis@secomm.org>
2018-04-04 11:38:02 -0400

    Initial commit.

---
+++ pippip-native-ios/Messages/ConversationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConversationViewController.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 4/4/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+import AsyncDisplayKit
+
+class ConversationViewController: AsyncMessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
