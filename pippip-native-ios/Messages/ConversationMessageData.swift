209be8ca0102b7250080e5a3da5232a4634c239f
Steve Brenneis <steve.brenneis@secomm.org>
2018-04-05 22:53:33 -0400

    Initial commit.

---
+++ pippip-native-ios/Messages/ConversationMessageData.swift
@@ -0,0 +1,62 @@
+//
+//  ConversationMessageData.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 4/5/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+
+class ConversationMessageData: MessageData {
+
+    var message: TextMessage
+    
+    init(message: TextMessage) {
+        self.message = message
+    }
+
+    func contentType() -> MessageDataContentType {
+        return kAMMessageDataContentTypeText
+    }
+    
+    func content() -> String {
+
+        if let cleartext = message.cleartext {
+            return cleartext
+        }
+        else {
+            return ""
+        }
+
+    }
+    
+    func date() -> Date {
+
+        let tsDouble = Double(message.timestamp)
+        return Date(timeIntervalSince1970: tsDouble / 1000)
+
+    }
+    
+    func senderID() -> String {
+        return message.publicId
+    }
+    
+    func senderDisplayName() -> String {
+
+        if let nickname = message.nickname {
+            return nickname
+        }
+        else {
+            return message.publicId
+        }
+
+    }
+    
+    func senderAvatarURL() -> URL {
+
+        return Bundle.main.url(forResource: "user", withExtension: nil)!
+
+    }
+    
+}
---
+++ pippip-native-ios/Support/MessageManager.swift
@@ -0,0 +1,49 @@
+//
+//  MessageManager.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 4/5/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+
+class MessageManager: RequestProcessProtocol {
+
+    var postPacket: PostPacket?
+    var errorDelegate: ErrorDelegate = NotificationErrorDelegate(title: "Message Error")
+
+    func sessionComplete(_ response: [AnyHashable : Any]) {
+        
+    }
+
+    func postComplete(_ response: [AnyHashable : Any]) {
+        
+    }
+
+    var messageDatabase = MessagesDatabase()
+
+    func getMessageIds(_ contactId: Int) -> [Int64] {
+
+        var messageIds = [Int64]()
+        let ids = messageDatabase.loadMessageIds(contactId)
+        for id in ids {
+            messageIds.append(id.int64Value)
+        }
+        return messageIds
+
+    }
+
+    func getTextMessages(_ contactId: Int) -> [TextMessage] {
+
+        let messageIds = getMessageIds(contactId)
+        var messages = [TextMessage]()
+        for messageId in messageIds {
+            let message = messageDatabase.loadTextMessage(Int(messageId), withContactId: contactId)
+            messages.append(message)
+        }
+        return messages
+
+    }
+
+}
