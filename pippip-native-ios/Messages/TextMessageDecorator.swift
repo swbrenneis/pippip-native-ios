64bc17dcd24f539e43c9ba69cc7310746a1fa7ea
Steve Brenneis <steve.brenneis@secomm.org>
2018-04-30 08:41:07 -0400

    Initial commit.

---
+++ pippip-native-ios/Messages/TextMessageDecorator.swift
@@ -0,0 +1,49 @@
+//
+//  TextMessageDecorator.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 4/27/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import Chatto
+import ChattoAdditions
+
+class TextMessageDecorator: ChatItemsDecoratorProtocol {
+
+    init() {
+    }
+
+    func decorateItems(_ chatItems: [ChatItemProtocol]) -> [DecoratedChatItem] {
+
+        var decoratedItems = [DecoratedChatItem]()
+        for index in 0..<chatItems.count {
+            let chatItem = chatItems[index] as! PippipTextMessageModel
+            var showingTail = true
+            var bottomMargin: CGFloat = 5.0
+            if index < chatItems.count - 1 {
+                let nextItem = chatItems[index + 1] as! PippipTextMessageModel
+                if chatItem.textMessage.originating == nextItem.textMessage.originating {
+                    showingTail = false
+                    bottomMargin = 1.0
+                }
+            }
+            let messageDecorationAttributes = BaseMessageDecorationAttributes(canShowFailedIcon: true,
+                                                                              isShowingTail: showingTail,
+                                                                              isShowingAvatar: false,
+                                                                              isShowingSelectionIndicator: false,
+                                                                              isSelected: false)
+            let decorationAttributes =
+                ChatItemDecorationAttributes(bottomMargin: bottomMargin,
+                                             messageDecorationAttributes: messageDecorationAttributes)
+            let decoratedItem = DecoratedChatItem(uid: chatItem.uid, chatItem: chatItem,
+                                                  decorationAttributes: decorationAttributes)
+
+            decoratedItems.append(decoratedItem)
+        }
+        return decoratedItems
+
+    }
+
+}
+
