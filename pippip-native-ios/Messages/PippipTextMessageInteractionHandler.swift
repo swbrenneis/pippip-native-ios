3ff5fa4d4cf295b365745f3b3ca8c344fa960827
Steve Brenneis <steve.brenneis@secomm.org>
2018-04-30 08:41:07 -0400

    Initial commit.

---
+++ pippip-native-ios/Messages/PippipTextMessageInteractionHandler.swift
@@ -0,0 +1,46 @@
+//
+//  PippipTextMessageInteractionHandler.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 4/27/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import Chatto
+import ChattoAdditions
+
+class PippipTextMessageInteractionHandler: BaseMessageInteractionHandlerProtocol {
+
+    init() {
+        
+    }
+
+    func userDidTapOnFailIcon(viewModel: PippipTextMessageViewModel, failIconView: UIView) {
+        
+    }
+
+    func userDidTapOnAvatar(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+    func userDidTapOnBubble(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+    func userDidBeginLongPressOnBubble(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+    func userDidEndLongPressOnBubble(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+    func userDidSelectMessage(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+    func userDidDeselectMessage(viewModel: PippipTextMessageViewModel) {
+        
+    }
+
+}
