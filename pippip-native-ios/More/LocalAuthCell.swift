b83b7d02d513cf91db07dc704ae0e12070667517
Steve Brenneis <steve.brenneis@secomm.org>
2018-05-04 19:41:07 -0400

    Initial commit.

---
+++ pippip-native-ios/More/LocalAuthCell.swift
@@ -0,0 +1,45 @@
+//
+//  LocalAuthCell.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 5/3/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+
+class LocalAuthCell: UITableViewCell {
+
+    @IBOutlet weak var localAuthSwitch: UISwitch!
+
+    var config = Configurator()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        localAuthSwitch.setOn(config.useLocalAuth(), animated: true)
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @objc class func cellItem() -> MoreCellItem {
+        
+        let item = MoreCellItem()
+        item.cellHeight = 65.0
+        item.cellReuseId = "LocalAuthCell"
+        return item
+        
+    }
+    
+    @IBAction func localAuthChanged(_ sender: UISwitch) {
+
+        config.useLocalAuth(sender.isOn)
+
+    }
+}
