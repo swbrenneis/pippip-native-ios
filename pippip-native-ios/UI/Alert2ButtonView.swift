d067310149d6f8da2e10e35bc9af2822278a2ba8
Steve Brenneis <steve.brenneis@secomm.org>
2018-08-20 10:31:13 -0400

    Initial commit.
    Amended commit email
    Amended commit email

---
+++ pippip-native-ios/UI/Alert2ButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  Alert2ButtonView.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 8/15/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+
+class Alert2ButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ pippip-native-ios/UI/ControllerBlurProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  ControllerBlurProtocol.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 8/15/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import Foundation
+
+protocol ControllerBlurProtocol {
+    
+    var blurView: UIVisualEffectView { get set }
+
+}
