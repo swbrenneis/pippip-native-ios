1dc11dcdaf7e4f5aff084b8294b159c52650aeed
Steve Brenneis <steve.brenneis@secomm.org>
2018-08-25 16:24:13 -0400

    Initial add.

---
+++ pippip-native-ios/Messages/PippipMessageCollectionViewCellStyle.swift
@@ -0,0 +1,15 @@
+//
+//  PippipMessageCollectionViewCellStyle.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 8/24/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+import Chatto
+import ChattoAdditions
+
+class PippipMessageCollectionViewCellStyle: BaseMessageCollectionViewCellDefaultStyle {
+
+}
---
+++ pippip-native-ios/Messages/PippipTextMessageViewModelBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  PippipTextMessageViewModelBuilder.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 8/23/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import UIKit
+import ChattoAdditions
+
+class PippipTextMessageViewModelBuilder: NSObject /* ViewModelBuilderProtocol */ {
+//    typealias ModelT = <#type#>
+    
+//    typealias ViewModelT = <#type#>
+    
+
+}
