1e2b7813d00b2883c27f088328db377a38060379
Steve Brenneis <steve.brenneis@secomm.org>
2018-11-30 15:53:53 -0500

    Initial commit.

---
+++ pippip-native-ios/API/SessionObserverProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  SessionObserverProtocl.swift
+//  pippip-native-ios
+//
+//  Created by Steve Brenneis on 11/29/18.
+//  Copyright © 2018 seComm. All rights reserved.
+//
+
+import Foundation
+
+protocol SessionObserverProtocol {
+    
+    func sessionStarted(sessionResponse: SessionResponse)
+
+}
